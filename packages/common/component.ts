9cccb23f55ba510c8570133a1059fc5582e4659d
bobiscool <vicentkarl@gmail.com>
2019-05-07 16:11:40 +0800

    百度小程序 引用组件貌似需要在页面里面声明

--- packages/common/component.ts
+++ packages/common/component.ts
@@ -49,6 +49,7 @@ function VantComponent<Data, Props, Watch, Methods, Computed>(
   options.behaviors = options.behaviors || [];
   options.behaviors.push(basic);
 
+  console.log('----编译组件---')
   // map field to form-field behavior
   if (vantOptions.field) {
     options.behaviors.push('s-//form-field');
